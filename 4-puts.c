860ed6574488348cc3af9571309a7d88366e1c5c
myUsername <myemail@gmail.com>
2023-02-16 14:13:50 -0800

    4-puts.c

--- 4-puts.c
+++ 4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - main block 
- * return: 0
+ * Return: o
  */
 int main(void)
-{   
-	put("\"Programming is like develop a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
